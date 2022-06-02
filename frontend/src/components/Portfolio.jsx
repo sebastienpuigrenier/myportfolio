e9dcbbe4b38efb00ff261e9a68083b0a20831c7a
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-06-03 01:05:51 +0200

    update

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -8,8 +8,72 @@ function Portfolio() {
   return (
     <section className="main-container portfolio">
       <h2 className="section-title">Porfolio</h2>
-      <div className="grid">
+      <div className="grid portfolio-container">
         <ul id="hexGrid">
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
+          <li>
+            <HexagonImg
+              title="Sébastien Puigrenier"
+              desc="Développeur Web"
+              link=""
+              imgBackgroundURL="../src/assets/HandyOldZ.png"
+            />
+          </li>
           <li>
             <HexagonImg
               title="Sébastien Puigrenier"

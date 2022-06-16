780cfe3ed321a590b9ae191cd21eebed8ff918ad
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-06-16 14:28:14 +0200

    ADD award on hex

--- frontend/src/components/Hexagon.jsx
+++ frontend/src/components/Hexagon.jsx
@@ -7,15 +7,17 @@ export default function Hexagon({ dim, l, ri, objectJSON, isfirstoddrow }) {
   };
 
   const hexTitleDynamicStyle = {
-    width: `${1.85 * l}${dim[1]}`,
     fontSize: `${0.15 * l}${dim[1]}`,
-    left: `${-1.3 * ri}${dim[1]}`,
+    width: `${1.85 * l}${dim[1]}`,
   };
 
   const hexTextDynamicStyle = {
-    left: `${-1.9 * ri}${dim[1]}`,
-    width: `${1.85 * l}${dim[1]}`,
     fontSize: `${0.1 * l}${dim[1]}`,
+  };
+
+  const hoverdivDynamicStyle = {
+    left: `${-1.1 * ri}${dim[1]}`,
+    height: `${0.9 * ri}${dim[1]}`,
   };
 
   return (
@@ -26,6 +28,7 @@ export default function Hexagon({ dim, l, ri, objectJSON, isfirstoddrow }) {
       {objectJSON.islink ? (
         <a href={objectJSON.link} target="_blank" rel="noreferrer">
           <img
+            className="backgroundimg"
             src={objectJSON.img}
             alt={objectJSON.title}
             style={imgDynamicStyle}
@@ -33,58 +36,71 @@ export default function Hexagon({ dim, l, ri, objectJSON, isfirstoddrow }) {
         </a>
       ) : (
         <img
+          className="backgroundimg"
           src={objectJSON.img}
           alt={objectJSON.title}
           style={imgDynamicStyle}
         />
       )}
-      <div>
-        {objectJSON.islink ? (
-          <a href={objectJSON.link} target="_blank" rel="noreferrer">
+      <div className="hoverdiv" style={hoverdivDynamicStyle}>
+        <div>
+          {objectJSON.islink ? (
+            <a href={objectJSON.link} target="_blank" rel="noreferrer">
+              <h1 className="hex-title" style={hexTitleDynamicStyle}>
+                {objectJSON.title}
+                <br /> {objectJSON.techno}
+              </h1>
+            </a>
+          ) : (
             <h1 className="hex-title" style={hexTitleDynamicStyle}>
               {objectJSON.title}
               <br /> {objectJSON.techno}
             </h1>
-          </a>
+          )}
+        </div>
+        {objectJSON.award ? (
+          <div className="hex-award">
+            <div className="hex-award-img">
+              <img src={objectJSON.awardimg} alt={objectJSON.awardtxt} />
+            </div>
+            <p className="infoaward">{objectJSON.awardtxt}</p>
+          </div>
         ) : (
-          <h1 className="hex-title" style={hexTitleDynamicStyle}>
-            {objectJSON.title}
-            <br /> {objectJSON.techno}
-          </h1>
+          <p />
         )}
-      </div>
-      <div>
-        <p className="hex-text" style={hexTextDynamicStyle}>
-          <span
-            style={
-              objectJSON.isgithublink
-                ? { display: "block" }
-                : { display: "none" }
-            }
-          >
-            <a href={objectJSON.github} target="_blank" rel="noreferrer">
-              GitHub
-            </a>
-          </span>
-          <span
-            style={
-              objectJSON.islink && objectJSON.isgithublink
-                ? { display: "block" }
-                : { display: "none" }
-            }
-          >
-            &nbsp;-&nbsp;
-          </span>
-          <span
-            style={
-              objectJSON.islink ? { display: "block" } : { display: "none" }
-            }
-          >
-            <a href={objectJSON.link} target="_blank" rel="noreferrer">
-              Lien
-            </a>
-          </span>
-        </p>
+        <div>
+          <p className="hex-text" style={hexTextDynamicStyle}>
+            <span
+              style={
+                objectJSON.isgithublink
+                  ? { display: "block" }
+                  : { display: "none" }
+              }
+            >
+              <a href={objectJSON.github} target="_blank" rel="noreferrer">
+                GitHub
+              </a>
+            </span>
+            <span
+              style={
+                objectJSON.islink && objectJSON.isgithublink
+                  ? { display: "block" }
+                  : { display: "none" }
+              }
+            >
+              &nbsp;-&nbsp;
+            </span>
+            <span
+              style={
+                objectJSON.islink ? { display: "block" } : { display: "none" }
+              }
+            >
+              <a href={objectJSON.link} target="_blank" rel="noreferrer">
+                Lien
+              </a>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
